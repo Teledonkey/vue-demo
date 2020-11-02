ca2abe69e6a363d69cd463ab36189bd46075b454
Teledonkey <changxin05@yunn.picc.com.cn>
2020-11-02 17:07:05 +0800

    lesson 9 completed

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <template v-for="menu in menuList">
+      <el-submenu
+        v-if="menu.children.length > 0"
+        :index="menu.path"
+        :key="menu.key"
+      >
+        <template slot="title">
+          <i :class="menu.icon"></i>
+          <span class="el-submenu__title" slot="title">{{ menu.label }}</span>
+        </template>
+        <menu-item :menuList="menu.children"></menu-item>
+      </el-submenu>
+      <el-menu-item v-else :index="menu.path" :key="menu.key">
+        <i :class="menu.icon"></i>
+        <span slot="title">{{ menu.label }}</span>
+      </el-menu-item>
+    </template>
+  </div>
+</template>
+
+<script>
+import MenuItem from "./MenuItem.vue";
+
+export default {
+  name: "MenuItem",
+  props: ["menuList"],
+  components: {
+    MenuItem,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.el-submenu__title {
+    font-size: 15px;
+    font-weight: 600;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container class="home">
+    <!-- 头部 -->
+    <el-header class="ub main-justify cross-center header">
+      <div class="header-left-text">Spring boot开发小而完整的Web前后端分离项目实战</div>
+      <div class="ub main-center cross-center">
+        <el-dropdown placement="bottom-start">
+          <img class="user-img" src="../assets/images/avatar.jpg" alt="">
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人中心</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <div class="user-role">
+          <div class="wollcom">欢迎您，管理员</div>
+          <div class="header-time">2020.11.02 10:29:23 星期一</div>
+        </div>
+      </div>
+    </el-header>
+    <el-container>
+      <!-- 菜单栏 -->
+      <el-aside width="auto">
+        <menu-bar></menu-bar>
+      </el-aside>
+      <el-container>
+        <!-- 右边内容显示区 -->
+        <el-main>Main</el-main>
+        <!-- <el-footer>Footer</el-footer> -->
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import MenuBar from "../components/MenuBar.vue"
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "Home",
+    components: {
+      MenuBar
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-.home {
-  height: 100%;
-}
+  .home {
+    height: 100%;
+  }
+
+  .header {
+    background-color: #167bd8;
+    color: white;
+  }
+
+  .header-left-text {
+    font-size: 20px;
+  }
+
+  .user-img {
+    height: 45px;
+    width: 45px;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  .user-role {
+    margin-left: 10px;
+  }
+
+  .wollcom {
+    font-size: 15px;
+    font-weight: bolder;
+  }
+
+  .header-time {
+    font-size: 14px;
+  }
+
+
 </style>
